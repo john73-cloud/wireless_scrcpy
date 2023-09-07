dbdd1ae4ca140f1090eb96835119ee3edb92d895
John Uguru <Johnuguru73@gmail.com>
2023-09-07 08:54:48 +0100

    editted commands

--- phone.bat
+++ phone.bat
@@ -1,6 +1,8 @@
-adb shell ifconfig rmnet4
+adb devices
+adb shell ip addr show rmnet0
 adb tcpip 5555
-set /p ip = put in inet addr:
+set /p ip = put in inet:
 adb connect %ip%
+echo please disconnect the usb device
 pause
 scrcpy
